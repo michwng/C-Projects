05bbd857bc85a22e8e50b5d423fc543b7728c196
Michael Ng <78869630+michwng@users.noreply.github.com>
2022-02-15 21:26:08 -0500

    Add files via upload

---
+++ C Projects/Lab 3 - C Classes/Lab 3 - C Classes/LastName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * An enum storing several common last names.
+ * 
+ * Date Created: 02/09/2020
+ */
+public enum LastName
+{
+    Smith,
+    Johnson,
+    Williams,
+    Brown,
+    Jones,
+    Miller,
+    Davis,
+    Garcia,
+    Rodriguez,
+    Wilson,
+    Martinez,
+    Anderson,
+    Taylor,
+    Thomas,
+    Hernandez,
+    Moore,
+    Martin,
+    Jackson,
+    Thompson,
+    White
+}
---
+++ C Projects/Lab2_C_Basics/README.txt
@@ -0,0 +1,6 @@
+A console application written in C# that can:
+1) Add 2 Numbers
+2) List a Multiplication Table (Based on input)
+3) List C# Type Information
+4) Detect if a word is a Palindrome
+5) Perform a Collatz conjecture.
