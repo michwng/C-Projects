09e401f6651f3e5d6519326bcb492d796f80f27e
Michael Ng <78869630+michwng@users.noreply.github.com>
2022-03-24 17:46:50 -0400

    Add files via upload

---
+++ Server Side Web Programming/Labs/Lab4-FileIOAndTextManipulation/Lab4-FileIOAndTextManipulation/Address.cs
@@ -0,0 +1,97 @@
+﻿/**       
+ * -------------------------------------------------------------------
+ * 	   File name: Address.cs
+ * 	Project name: Lab4-FileIOAndTextManipulation
+ * -------------------------------------------------------------------
+ * Author’s name and email:	Michael Ng, ngmw01@etsu.edu			
+ *          Course-Section: CSCI-2910-001
+ *           Creation Date:	02/15/2022	
+ *           Last Modified: 02/17/2022
+ * -------------------------------------------------------------------
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_FileIOAndTextManipulation
+{
+    public class Address
+    {
+        //Fields for the Address class.
+        int Number { get; init; }
+        string Street { get; init; }
+        string? County { get; init; }
+        string City { get; init; }
+        State State { get; init; }
+        int ZipCode { get; init; }
+
+        /*
+         * The constructor for the Address class.
+         * 
+         * Date Created: 02/17/2022
+         * @param number, ZipCode (int)
+         * @param city, state (string)
+         */
+        public Address(string address, string city, string state, string zipCode, string? county) 
+        {
+            //We will need to format address to get number and street.
+            string[] addressParts = address.Split(' ');
+
+            //The number comes first, then the street.
+            Number = int.Parse(addressParts[0]);
+
+            //Assemble the street using a for-loop.
+            StringBuilder street = new StringBuilder();
+            for (int i = 1; i < addressParts.Length; i++) 
+            {
+                street.Append(addressParts[i]);
+            }
+
+            County = county;
+
+            Street = street.ToString();
+
+            City = city;
+
+            State = new State(state);
+
+            ZipCode = int.Parse(zipCode);
+        }
+
+
+        /*
+         * The overriding ToString for the Address class.
+         * 
+         * Date Created: 02/17/2022
+         * @return $"{Number} {Street} | {City} {State.FullName}, {ZipCode}"
+         */
+        public override string ToString() 
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{Number} {Street} | {City} {State.FullName}, {ZipCode}");
+
+            //If County is not null, we append it to sb.
+            if (County != null) 
+            {
+                sb.Append($" | {County}");
+            }
+
+            return sb.ToString();
+        }
+
+
+        /*
+         * The overriding ToString for the Address class.
+         * 
+         * Date Created: 02/17/2022
+         * @return $"{Number} {Street} | {City} {State.FullName}, {ZipCode}"
+         */
+        public string ShortToString()
+        {
+            return $"{City}, {State.FullName}";
+        }
+
+    }
+}
---
+++ Server Side Web Programming/Labs/Lab4-FileIOAndTextManipulation/Lab4-FileIOAndTextManipulation/FileRoot.cs
@@ -0,0 +1,48 @@
+﻿/**       
+ * -------------------------------------------------------------------
+ * 	   File name: FileRoot.cs
+ * 	Project name: Lab4-FileIOAndTextManipulation
+ * -------------------------------------------------------------------
+ * Author’s name and email:	Michael Ng, ngmw01@etsu.edu			
+ *          Course-Section: CSCI-2910-001
+ *           Creation Date:	02/15/2022	
+ *           Last Modified: 02/16/2022
+ * -------------------------------------------------------------------
+ */
+using Lab4_FileIOAndTextManipulation;
+using System;
+using System.IO;
+
+
+namespace Lab4_FileIOAndTextManipulation
+{
+    public class FileRoot
+    {
+        //Fields for the FileRoot Class
+        string path { get; init; }
+        char separatorChar { get; init; }
+
+        /**
+         * The constructor of the FileRoot class.
+         * Initializes the path to the top-level directory.
+         * 
+         * Date Created: 02/15/2022
+         */
+        public FileRoot() 
+        {
+            path = (Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent).ToString();
+            separatorChar = Path.DirectorySeparatorChar;
+        }
+
+        /**
+         * Overrides the ToString method and
+         * returns the path instead.
+         * 
+         * Date Created: 02/16/2022
+         */
+        public override string ToString() 
+        {
+            return path;
+        }
+    }
+}
