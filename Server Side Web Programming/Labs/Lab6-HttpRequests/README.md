33faec392baaddb750bc4afc38e3eacfc7db6e97
Michael Ng <78869630+michwng@users.noreply.github.com>
2022-08-19 16:25:56 -0400

    Add files via upload

---
+++ Server Side Web Programming/Labs/Lab6-HttpRequests/README.md
@@ -0,0 +1,4 @@
+Lab 6 teaches us about making HTTP Requests in a .NET Console using C#.
+ - This app makes web requests and parses the results.
+ - Derived directly from Microsoft Docs:
+ - https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
---
+++ Server Side Web Programming/Labs/Lab6-HttpRequests/WebAPIClient/repo.cs
@@ -0,0 +1,56 @@
+﻿/**       
+ * -------------------------------------------------------------------
+ * 	   File name: repo.cs
+ * 	Project name: WebAPIClient
+ * -------------------------------------------------------------------
+ *  Author’s name and email:    Michael Ng, ngmw01@etsu.edu			
+ *            Creation Date:	03/30/2022	
+ *            Last Modified:    03/30/2022
+ * -------------------------------------------------------------------
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace WebAPIClient
+{
+    //This class is meant to represent the JSON object returned from the GitHub API.
+    //This class displays a list of repository names.
+    public class Repository
+    {
+        //With this addition, Name appears as "name" in JSON.
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        //With this addition, Description appears as "description" in JSON.
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        //GitHubHomeUrl appears as "html_url" in JSON.
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        //Homepage appears as "homepage" in JSON.
+        [JsonPropertyName("homepage")]
+        public Uri Homepage { get; set; }
+
+        //Watchers appears as "watchers" in JSON.
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        //The Uri and int types can convert to and from string representation.
+        //No extra code is needed to deserialize from JSON string format
+        //to those target types.
+
+        //Date is formatted as this in JSON: YYYY-MM-DDTHH:MM:SSZ
+        //For example: 2016-02-08T21:27:00Z.
+        //LastPushUtc appears as "pushed_at" in JSON.
+        [JsonPropertyName("pushed_at")]
+        public DateTime LastPushUtc { get; set; }
+        public DateTime LastPush => LastPushUtc.ToLocalTime();
+    }
+}
